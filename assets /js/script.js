23b9bc12e48c31e83b65380e22ee0a865ed3bb74
nicolewallace09 <nicole.elisaw@gmail.com>
2020-04-01 20:44:27 -0700

    form button

--- assets /js/script.js
+++ assets /js/script.js
@@ -1,9 +1,12 @@
 console.dir(window.document);
 
-var buttonEl = document.querySelector('#save-task');
+var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector('#tasks-to-do');
 
-var createTaskHandler = function () { 
+var createTaskHandler = function (event) { 
+
+    event.preventDefault();
+
     var listItemEl = document.createElement('li');
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
@@ -12,7 +15,7 @@ var createTaskHandler = function () {
 
 
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("click", createTaskHandler);
 
 
 
